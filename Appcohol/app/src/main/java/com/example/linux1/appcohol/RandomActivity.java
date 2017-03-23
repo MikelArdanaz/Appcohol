b574786e2edd52cf539deeac7e2766e69ebc608e
MikelArdanaz <mikelillo_1@msn.com>
2017-03-24 00:50:44 +0100

    Esbozo de RandomActivity

--- Appcohol/app/src/main/java/com/example/linux1/appcohol/RandomActivity.java
+++ Appcohol/app/src/main/java/com/example/linux1/appcohol/RandomActivity.java
@@ -5,10 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 public class RandomActivity extends AppCompatActivity {
 
     /* Elementos del layout */
     private Button bt_aleatorio;
+    private List<ParseObject> lista_objetos;
 
     /* Variables */
 
@@ -26,7 +33,13 @@ public class RandomActivity extends AppCompatActivity {
         bt_aleatorio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("cockteles");
+                try {
+                    lista_objetos = query.find();
+                    int indice = ((int) Math.random() * lista_objetos.size()-1); //Los
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
