01a33b1124e11e35fd0041e05d7e3388115d22ab
MikelArdanaz <mikelillo_1@msn.com>
2017-03-25 17:16:39 +0100

    Progresos en MapaActivity

--- Appcohol/app/src/main/java/com/example/linux1/appcohol/MapaActivity.java
+++ Appcohol/app/src/main/java/com/example/linux1/appcohol/MapaActivity.java
@@ -4,19 +4,22 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapzen.android.lost.api.LocationServices;
+
 
 public class MapaActivity extends AppCompatActivity {
 
@@ -25,17 +28,23 @@ public class MapaActivity extends AppCompatActivity {
     private Button bt_volver;
 
     /* Variables */
+    private MapView mapView = null;
     private MapboxMap map = null;
     private LocationServices locationServices;
+    private FloatingActionButton floatingActionButton;
+    private static final int PERMISSIONS_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, getString(R.string.access_token));
+        MapboxAccountManager.start(this, getString(R.string.access_token));
         setContentView(R.layout.activity_mapa);
 
+
+        locationServices = LocationServices.getLocationServices(MapaActivity.this);
+
+
         /* Relacionar elementos del layout */
-        mapa = (MapView)findViewById(R.id.mv_mapa_mapa);
         bt_volver = (Button) findViewById(R.id.bt_mapa_volver);
 
         bt_volver.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +54,18 @@ public class MapaActivity extends AppCompatActivity {
             }
         });
 
-        mapa.onCreate(savedInstanceState);
-        mapa.getMapAsync(new OnMapReadyCallback() {
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.Bt_Location);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+                }
+            }
+        });
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
 
@@ -65,43 +84,85 @@ public class MapaActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        mapa.onResume();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapa.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapa.onStop();
+        mapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapa.onPause();
+        mapView.onPause();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapa.onLowMemory();
+        mapView.onLowMemory();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mapa.onDestroy();
+        mapView.onDestroy();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapa.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    private void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            // Check if user has granted location permission
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            // If we have the last location of the user, we can move the camera to that position.
+            Location lastLocation = locationServices.getLastLocation();
+            if (lastLocation != null) {
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+            }
+
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        // Move the map camera to where the user location is and then remove the
+                        // listener so the camera isn't constantly updating when the user location
+                        // changes. When the user disables and then enables the location again, this
+                        // listener is registered again and will adjust the camera once again.
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
+                        locationServices.removeLocationListener(this);
+                    }
+                }
+            });
+            floatingActionButton.setImageResource(R.drawable.ic_location_disabled_24dp);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.ic_my_location_24dp);
+        }
+        // Enable or disable the location layer on the map
+        map.setMyLocationEnabled(enabled);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableLocation(true);
+            }
+        }
     }
 
 }
